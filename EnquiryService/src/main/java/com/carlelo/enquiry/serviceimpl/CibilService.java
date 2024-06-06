3b3eabbd0cdfccc2b423e01635dd133c82543b9a
T L Solution <T L Solution@DESKTOP-RSIOBQ1>
2024-06-06 14:41:03 +0530

    ok

---
+++ EnquiryService/src/main/java/com/carlelo/enquiry/serviceimpl/CibilService.java
@@ -0,0 +1,23 @@
+package com.carlelo.enquiry.serviceimpl;
+
+import java.util.Random;
+
+import com.carlelo.enquiry.model.CibilDetails;
+
+public class CibilService 
+{
+	//applicable
+	public String isApplicable(int score)
+	{
+		if (score <- 600)
+		{
+			return "NO";
+		}
+		else 
+		{
+			return "YES"; 
+		}
+	}
+	
+	
+}
